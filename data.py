22694510cdcda18c8be5478873c83b1e2d9b1a3f
Anastasiia Mironova <m1rona.a@yandex.ru>
2026-01-01 23:37:16 +0300

    add main link in urls and language data helpers

---
+++ data.py
@@ -0,0 +1,29 @@
+class Urls:
+    link = "http://selenium1py.pythonanywhere.com/"
+
+
+class Language:
+    langs = [
+        "ar",
+        "ca",
+        "cs",
+        "da",
+        "de",
+        "en",
+        "el",
+        "es",
+        "fi",
+        "fr",
+        "it",
+        "ko",
+        "nl",
+        "pl",
+        "pt",
+        "pt-br",
+        "ro",
+        "ru",
+        "sk",
+        "uk",
+        "zh-hans",
+    ]
+
