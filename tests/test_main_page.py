3b835d731e3133774551d1cb34e4d18404737987
Anastasiia Mironova <m1rona.a@yandex.ru>
2026-01-02 00:28:28 +0300

    add main page object and refactor test to use POM

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,9 +1,10 @@
-from selenium.webdriver.common.by import By
 from stepic_pom_autotesting.data import Urls as U
+from stepic_pom_autotesting.pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
-    browser.get(U.link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+    link = U.link
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
 
