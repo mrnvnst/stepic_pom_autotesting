545e3591b19d6f6df31d540015bb9d9c6024fb95
Anastasiia Mironova <m1rona.a@yandex.ru>
2026-01-02 19:52:31 +0300

    add test to verify login link visibility

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -8,3 +8,10 @@ def test_guest_can_go_to_login_page(browser):
     page.open()
     page.go_to_login_page()
 
+
+def test_guest_should_see_login_link(browser):
+    link = U.link
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
